60287a5891d6a9bfe032b98db9baaf47910ecc7f
Beka Chogovadze <123267327+JavaEnergy@users.noreply.github.com>
2023-12-07 21:53:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const firstQuestion = document.getElementById("firstQuestion");
+const secondQuestion = document.getElementById("secondQuestion");
+const thirdQuestion = document.getElementById("thirdQuestion");
+const forthQuestion = document.getElementById("forthQuestion");
+
+const firstPlusIcon = document.getElementById("firstPlusIcon");
+const secondPlusIcon = document.getElementById("secondPlusIcon");
+const thirdPlusIcon = document.getElementById("thirdPlusIcon");
+const forthPlusIcon = document.getElementById("forthPlusIcon");
+
+const firstMinusIcon = document.getElementById("firstMinusIcon");
+const SecondMinusIcon = document.getElementById("SecondMinusIcon");
+const thirdMinusIcon = document.getElementById("thirdMinusIcon");
+const forthMinusIcon = document.getElementById("forthMinusIcon");
+
+const firstAnswer = document.getElementById("firstAnswer");
+const secondAnswer = document.getElementById("secondAnswer");
+const thirdAnswer = document.getElementById("thirdAnswer");
+const forthAnswer = document.getElementById("forthAnswer");
+
+firstQuestion.addEventListener("click", () => {
+  if (firstPlusIcon.style.display == "block") {
+    firstPlusIcon.style.display = "none";
+    firstMinusIcon.style.display = "block";
+    firstAnswer.style.display = "block";
+  } else {
+    firstPlusIcon.style.display = "block";
+    firstMinusIcon.style.display = "none";
+    firstAnswer.style.display = "none";
+  }
+});
+secondQuestion.addEventListener("click", () => {
+  if (secondPlusIcon.style.display == "block") {
+    secondPlusIcon.style.display = "none";
+    SecondMinusIcon.style.display = "block";
+    secondAnswer.style.display = "block";
+  } else {
+    secondPlusIcon.style.display = "block";
+    SecondMinusIcon.style.display = "none";
+    secondAnswer.style.display = "none";
+  }
+});
+thirdQuestion.addEventListener("click", () => {
+  if (thirdPlusIcon.style.display == "block") {
+    thirdPlusIcon.style.display = "none";
+    thirdMinusIcon.style.display = "block";
+    thirdAnswer.style.display = "block";
+  } else {
+    thirdPlusIcon.style.display = "block";
+    thirdMinusIcon.style.display = "none";
+    thirdAnswer.style.display = "none";
+  }
+});
+forthQuestion.addEventListener("click", () => {
+  if (forthPlusIcon.style.display == "block") {
+    forthPlusIcon.style.display = "none";
+    forthMinusIcon.style.display = "block";
+    forthAnswer.style.display = "block";
+  } else {
+    forthPlusIcon.style.display = "block";
+    forthMinusIcon.style.display = "none";
+    forthAnswer.style.display = "none";
+  }
+});
